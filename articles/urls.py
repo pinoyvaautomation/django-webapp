66f390cd8c6e2479f04313301a08fa280bc2ae24
pinoyvaautomation <pinoyvaautomation@gmail.com>
2025-07-02 17:29:54 +0800

    Added search function

--- articles/urls.py
+++ articles/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('articles/', views.article_list, name='articlelist'),
     path('<int:article_id>/', views.article_detail, name='article_detail'),
     path('search/', views.article_search, name='article_search'),
+
+    path('ajax/search/', views.ajax_search_articles, name='ajax_search_articles')
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,8 +3,10 @@
 # Create your views here.
 from django.shortcuts import render, get_object_or_404 # type: ignore
 from .models import Article, Category
+from shop.models import Product
+from download_library.models import Customer, File
 from django.db.models import Q # type: ignore
-from django.http import HttpResponse # type: ignore
+from django.http import HttpResponse, JsonResponse # type: ignore
 
 def HomePage(request):
     return render(request, 'master.html')
@@ -31,3 +33,57 @@ def article_search(request):
     else:
         articles = Article.objects.none()
     return render(request, 'article_search.html', {'articles': articles, 'query': query})
+
+#SEARCH QUERY
+
+def search_articles(request):
+    query = request.GET.get('q')
+    results = []
+
+    if query:
+        results = Article.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+        ).distinct()
+
+    return render(request, 'search_results.html', {
+        'query': query,
+        'results': results
+    })
+
+
+def ajax_search_articles(request):
+    query = request.GET.get('q', '')
+    #results = []
+    data = {
+        'articles': [],
+        'products': [],
+        'customers': []
+    }
+
+    if query:
+        articles = Article.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
+
+        products = Product.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+
+        customers = Customer.objects.filter(
+            Q(name__icontains=query) | Q(email__icontains=query)
+        )
+
+        data['articles'] = [
+            {'id': a.id, 'title': a.title, 'snippet': a.content[:100]} for a in articles
+        ]
+
+        data['products'] = [
+            {'id': p.id, 'name': p.name, 'desc': p.description[:100]} for p in products
+        ]
+
+        data['customers'] = [
+            {'id': c.id, 'name': c.name, 'desc': c.email[:100]} for c in customers
+        ]
+
+    return JsonResponse(data)
